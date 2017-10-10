209f47cebfd3edc2d6e2b974b0f5b30b5275aa2e
NicolaiVBonderup <nvbonderup@gmail.com>
2017-10-10 11:57:26 +0200

    Added form for posting a comment to a post.
    Added API call to backend for posting a comment to a post.

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Post} from '../../models/post';
+import {CommentSubmission, Comment} from '../../models/comment';
+import {ApiService} from '../../services/api/api.service';
+import {TokenService} from '../../services/token/token.service';
+import {UserService} from 'app/services/user/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -10,14 +15,61 @@ import {Post} from '../../models/post';
 export class PostComponent implements OnInit {
 
   post: Post;
+  loading: boolean;
+  error: string = null;
+  submitted: boolean;
+  commentField: CommentSubmission;
+  comments: Comment;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private apiService: ApiService,
+              private tokenService: TokenService,
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit() {
       const response = this.route.snapshot.data['post'];
       if (response.data) {
           this.post = response.data;
       }
+
+      const currentUser = this.userService.getUser();
+      if (currentUser != null) {
+          this.commentField = new CommentSubmission(currentUser.id, this.post.id, 0, '');
+      } else {
+          this.commentField = new CommentSubmission(0, this.post.id, 0, '');
+      }
   }
+
+  onSubmit() {
+      if (this.commentField.user_ref !== 0) {
+          this.submitted = true;
+          this.error = null;
+          this.loading = true;
+          this.postCommentToPost();
+      } else {
+          this.error = 'You must be logged in to post a comment.';
+      }
+  }
+
+    public postCommentToPost(): void {
+        const token = this.tokenService.getToken();
+
+        this.apiService.postComment(this.commentField.user_ref, this.commentField.post_ref,
+                                    this.commentField.comment_ref, this.commentField.content, token)
+            .subscribe((res) => {
+                this.loading = false;
+                this.commentField.content = '';
+            }, (error) => {
+                this.loading = false;
+                if (error.hasOwnProperty('error')) {
+                    const errorResponse = JSON.parse(error.error);
+                    if (errorResponse.hasOwnProperty('message')) {
+                        this.error = errorResponse.message;
+                    }
+                }
+            });
+
+    }
 
 }
