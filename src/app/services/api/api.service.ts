5904bffc06bc793bf21f5f3f9895558080b6e5c9
Ebski <Ebski91@gmail.com>
2017-10-23 09:15:22 +0200

    Changed routes in frontend

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -27,25 +27,25 @@ export class ApiService {
     }
 
     getPosts(limit, page): Observable<any> {
-        return this.http.get(this.apiUrl + `/hackerpost?limit=10&page=${page}`);
+        return this.http.get(this.apiUrl + `/posts?limit=10&page=${page}`);
     }
 
     getPost(slug: string): Observable<any> {
-        return this.http.get(this.apiUrl + `/hackerpost/${slug}`);
+        return this.http.get(this.apiUrl + `/posts/${slug}`);
     }
 
     upvotePost(postId: number, token): Observable<any> {
         const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-        return this.http.post(this.apiUrl + `/upvotepost`, {postRef: postId}, {headers});
+        return this.http.post(this.apiUrl + `/posts/upvote`, {post_ref: postId}, {headers});
     }
 
     downvotePost(postId: number, token): Observable<any> {
         const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-        return this.http.post(this.apiUrl + '/downvotepost', {postRef: postId}, {headers});
+        return this.http.post(this.apiUrl + '/posts/downvote', {post_ref: postId}, {headers});
     }
 
     postComment(post_ref, comment_ref, content, token): Observable<any> {
         const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-        return this.http.post(this.apiUrl + '/comment', {post_ref, comment_ref, content}, {headers});
+        return this.http.post(this.apiUrl + '/comments', {post_ref, comment_ref, content}, {headers});
     }
 }
